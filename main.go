2de52d6a7a610e1d7f98377d5bd19a2ee4cf03d8
fatahnuram <fatahnuram@gmail.com>
2024-06-10 17:16:41 +0700

    feat: handler using func

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
+
+func getTime(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(time.Now().String()))
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world!"))
 	})
+	http.HandleFunc("/time", getTime)
 
 	log.Println("web server started at localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
