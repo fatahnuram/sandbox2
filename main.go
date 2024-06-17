cdc9294b81ef0e306cd75d8e2a871558369d7e73
fatahnuram <fatahnuram@gmail.com>
2024-06-17 22:49:29 +0700

    feat: logger middleware

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"path"
 	"strings"
+	"time"
 )
+
+type StatusResponseWriter struct {
+	http.ResponseWriter
+	Status       int
+	OriginalPath string
+}
+
+func (w *StatusResponseWriter) WriteHeader(statuscode int) {
+	w.Status = statuscode
+	w.ResponseWriter.WriteHeader(statuscode)
+}
+
+func (w *StatusResponseWriter) Write(b []byte) (int, error) {
+	if w.Status == 0 {
+		w.Status = http.StatusOK
+	}
+	return w.ResponseWriter.Write(b)
+}
 
 func shiftPath(p string) (string, string) {
 	p = path.Clean("/" + p)
@@ -16,6 +36,20 @@ func shiftPath(p string) (string, string) {
 	return p[1:i], p[i:]
 }
 
+func withLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		srw := &StatusResponseWriter{
+			ResponseWriter: w,
+			OriginalPath:   r.URL.Path,
+		}
+
+		start := time.Now()
+		next.ServeHTTP(srw, r)
+		end := time.Since(start)
+		fmt.Printf("%s %d %s %v\n", r.Method, srw.Status, srw.OriginalPath, end)
+	})
+}
+
 func main() {
 	app := &App{
 		User:    &Route{Handler: &User{}},
@@ -23,7 +57,7 @@ func main() {
 	}
 
 	log.Println("web server started at localhost:8080")
-	if err := http.ListenAndServe(":8080", app); err != nil {
+	if err := http.ListenAndServe(":8080", withLogger(app)); err != nil {
 		log.Fatal("unable to start web server", err)
 	}
 }
